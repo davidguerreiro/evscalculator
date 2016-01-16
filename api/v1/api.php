f0700a6c87f957defb78a1fc6f27adcc1bdce1e5
Jaime Caballero <lamatesoft@gmail.com>
2016-01-16 17:16:14 +0000

    Hordes GET done

--- api/v1/api.php
+++ api/v1/api.php
@@ -2,14 +2,19 @@
 require 'lib/functions.php';
 
 
+
 // Generic
-$app->get('/', 'goHome');
+$app->get('/', function($req, $res){
+    return $res->withStatus(301)->withHeader('Location', 'https://evscalculator.com');
+});
 
 
 // Hordes
-$app->get('/v1/hello/{name}', function ($request, $response) {
-    $name = $request->getAttribute('name');
-    $response->getBody()->write("Hello, $name");
+$app->get('/v1/hordes', function ($req, $res) {
 
-    return $response;
+    $res->getBody()->write(json_encode(
+        getHordes($req->getQueryParams()['stat'], $req->getQueryParams()['game'])
+    ));
+
+    return $res->withHeader('Content-type', 'application/json');
 });
--- api/v1/lib/functions.php
+++ api/v1/lib/functions.php
@@ -1,5 +1,56 @@
 <?php
 
-function goHome() {
-	header('Location: https://evscalculator.com');
+
+
+
+function isStat($stat) {
+    if(!$stat) return false;
+
+    $stat_list = array(
+        'hp', 
+        'attack', 
+        'defense', 
+        'spattack', 
+        'spdefense', 
+        'speed'
+    );
+
+    return in_array($stat, $stat_list);
+}
+
+
+function isGame($game) {
+
+    $game_list = array(
+        0,
+        1
+    );
+
+    return in_array($game, $game_list);
+}
+
+
+function getHordes($stat = false, $game = false) {
+
+    // Grab JSON and make it an array of objects
+    $ret = json_decode(file_get_contents('./v1/data/hordes.json'));
+
+    // Filter by stat
+    if(isStat($stat)) {
+        $ret = array_filter($ret, function($a) use($stat) {
+            return $a->stat_name == $stat;
+        });
+    }
+
+    // Filter by game
+    if(isGame($game)) {
+        $ret = array_filter($ret, function($a) use($game) {
+            return $a->game == $game;
+        });
+    }
+
+    // Remove original indexes
+    $ret = array_values($ret);
+
+    return $ret;
 }
