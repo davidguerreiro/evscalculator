a2edd79f3fd5a0d0cf39386a84fae283645303f2
Jaime Caballero <me@jaicab.com>
2016-01-22 16:15:19 +0000

    Simplify CORS

--- api/index.php
+++ api/index.php
@@ -1,15 +1,14 @@
 <?php
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-}
 
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require 'vendor/autoload.php';
 
-$app = new \Slim\App;
 
+header("Access-Control-Allow-Origin: *");
+
+$app = new \Slim\App;
 
 require 'v1/api.php';
 
