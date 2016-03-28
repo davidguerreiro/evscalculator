85e6432fddf415f829e046f787acc9f134c00e9f
Jaime Caballero <lamatesoft@gmail.com>
2016-03-28 21:18:43 +0100

    Try cURL with cert

--- lib/EVsApi.php
+++ lib/EVsApi.php
@@ -26,7 +26,6 @@ class EVs {
 		if(!empty($stat) && $game !== false)
 			$petition_url .= '?stat='.$stat.'&game='.$game;
 		else{
-
 			if(!empty($stat))
 				$petition_url .= '?stat='.$stat;
 
@@ -132,6 +131,11 @@ class EVs {
 		$curl = curl_init();
 		curl_setopt($curl, CURLOPT_URL, $petition_url);
 		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+
+		curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
+		curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
+		curl_setopt($curl, CURLOPT_CAINFO, "Api.crt");
+
 		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
 		//exc
