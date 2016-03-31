cd8ff73adcdfb3a41718cf259b082c6bb2dc6b14
Jaime Caballero <lamatesoft@gmail.com>
2016-03-31 08:18:16 +0100

    Add classes to stat fields

--- src/js/components/StatValidator.js
+++ src/js/components/StatValidator.js
@@ -14,6 +14,8 @@ var StatValidator = (function(doc) {
 		if(!this.el.value) {
 			if((this.wrap.maxEvs - this.wrap.statTotal()) > 252 ) this.el.value = 252;
 			else this.el.value = (this.wrap.maxEvs - this.wrap.statTotal());
+			// Active class
+			if(!this.el.classList.contains('is-active')) this.el.classList.add('is-active');
 		}
 		if(window.graphdata) {
 			window.graphdata[this.el.name] = parseInt(this.el.value, 10);
@@ -23,7 +25,10 @@ var StatValidator = (function(doc) {
 	};
 
 	Stat.prototype.statBlur = function() {
-		if(this.el.value == 0) this.el.value = '';
+		if(this.el.value == 0) {
+			this.el.value = '';
+			if(this.el.classList.contains('is-active')) this.el.classList.remove('is-active');
+		}
 		else {
 			if(parseInt(this.el.value, 10) > this.el.max) this.el.value = this.el.max;
 			this.el.value = Math.ceil(parseInt(this.el.value)/4.0) * 4;
